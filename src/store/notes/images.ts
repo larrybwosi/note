62963bbdb492bfd12327cd0976cd3a649ac288f1
larrybwosi <larrybwosi@gmail.com>
2025-01-13 15:43:14 +0300

    removed all the other pages including schedule and notes

--- src/store/notes/images.ts
+++
@@ -1,5 +0,0 @@
-export enum NoteImages {
-  Image1 = 'https://cdn.sanity.io/images/kecd6xb4/production/dd0a8a1240786bd911fa7fe9ac0d73c359ce86ef-771x510.png?w=400&h=400&fit=crop&fm=webp',
-  Image2 = 'https://cdn.sanity.io/images/kecd6xb4/production/1710c9a8cdbcfe3dbd10b130d9bae608ec6acb57-1449x815.png?w=400&h=400&fit=crop&fm=webp',
-  Image3 = 'https://cdn.sanity.io/images/kecd6xb4/production/dd0a8a1240786bd911fa7fe9ac0d73c359ce86ef-771x510.png?w=400&h=400&fit=crop&fm=webp',
-}
